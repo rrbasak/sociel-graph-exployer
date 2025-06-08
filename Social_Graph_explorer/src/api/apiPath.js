ed68100aef12d85903f93ee02afea7290e2c6c1c
Rajdeep Basak <rajdeepbasak2000@gmail.com>
2025-06-09 02:05:44 +0530

    Modified base url

--- Social_Graph_explorer/src/api/apiPath.js
+++ Social_Graph_explorer/src/api/apiPath.js
@@ -1,1 +1,2 @@
-export const BASE_URL = "http://localhost:9003/api/v0"; 
+// export const BASE_URL = "http://localhost:9003/api/v0"; 
+export const BASE_URL = "https://social-graph-exployer-backend-api-gateway.onrender.com/api/v0"; 

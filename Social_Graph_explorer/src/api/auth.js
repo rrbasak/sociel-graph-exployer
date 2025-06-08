e2c3a24ffdbabc7ef1187ab7a808c34634bfaca6
Rajdeep Basak <rajdeepbasak2000@gmail.com>
2025-06-09 02:27:58 +0530

    Changed env variable from REACT_APP to VITE

--- Social_Graph_explorer/src/api/auth.js
+++ Social_Graph_explorer/src/api/auth.js
@@ -211,12 +211,12 @@ export const getPendingRequest = async (userId) => {
 export const uploadImageToCloudinary = async (file) => {
   const formData = new FormData();
   formData.append("file", file);
-  formData.append("upload_preset", process.env.VITE_CLOUDINARY_UPLOAD_PRESET);
-  formData.append("cloud_name", process.env.VITE_CLOUDINARY_CLOUD_NAME);
+  formData.append("upload_preset", import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET);
+  formData.append("cloud_name", import.meta.env.VITE_CLOUDINARY_CLOUD_NAME);
 
   try {
     const res = await axiosInstance.post(
-      `https://api.cloudinary.com/v1_1/${process.env.VITE_CLOUDINARY_CLOUD_NAME}/image/upload`,
+      `https://api.cloudinary.com/v1_1/${import.meta.env.VITE_CLOUDINARY_CLOUD_NAME}/image/upload`,
       formData
     );
 
